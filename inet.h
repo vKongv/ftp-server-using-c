122e5f042de5f1423ab22a391227e8b0b16002d9
vKongv <kaweng456@hotmail.com>
2015-09-24 01:40:02 +0800

    Create inet.h

---
+++ inet.h
@@ -0,0 +1,17 @@
+/*inet.h*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define SERV_TCP_PORT 25000
+#define SERV_UDP_PORT 35001
+#define CLI_UDP_PORT 35002
+
+const char *ERR_MSG = "Cannot open the file\n";
+const char *CRASH_MSG = "Unknown Error\n";
