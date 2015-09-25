53b54e0ca4b609a957ff4bec8f634946a5e71979
vKongv <kaweng456@hotmail.com>
2015-09-26 01:47:29 +0800

    Update inet.h

--- inet.h
+++ inet.h
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <dirent.h>
 #include <pwd.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
@@ -15,6 +16,6 @@
 #define SERV_UDP_PORT 35001
 #define CLI_UDP_PORT 35002
 
-const char *ERR_MSG[] = {"Cannot open the file\n", "Unknown Error\n"};
+const char *ERR_MSG[] = {"Cannot open the file\n", "Unknown Error\n", "Fail to upload\n"};
 const char *CRASH_MSG = "Unknown Error\n";
 const char *NULL_MSG = "NULL";
